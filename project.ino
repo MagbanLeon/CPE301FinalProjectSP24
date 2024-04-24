ba4339bc42e1e456284760c9cb1afeccfe066809
Leon Magbanua <112917169+MagbanLeon@users.noreply.github.com>
2024-04-24 17:47:10 +0000

    libraries

--- project.ino
+++ project.ino
@@ -1,7 +1,11 @@
+//#include <LiquidCrystal.h> //For LCD: https://www.arduino.cc/reference/en/libraries/liquidcrystal/
+//#include <Stepper.h> //For stepper motor: https://www.arduino.cc/reference/en/libraries/stepper/
+
+
 void setup(){
 
 }
 
 void loop(){
-    
+
 }
